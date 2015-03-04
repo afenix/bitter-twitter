87df96ca8426ec07b12d458612433f8c61642862
J. TURNER <turner.j6@gmail.com>
2015-03-03 17:53:26 -0800

    Update files to impliment 'sortByDate' functionality.

--- controllers/beets.js
+++ controllers/beets.js
@@ -1,7 +1,18 @@
 Bitter.BeetsController = Ember.ArrayController.extend({
   actions: {
     addBeet: function() {
-      beets.addObject({id: this.length+1, user: this.user, message: this.message});
+      beets.addObject({
+                      id: this.length+1,
+                      user: this.user,
+                      message: this.message,
+                      date: new Date,
+                      comments: new Array
+                      });
+      this.set("user", "");
+      this.set("message", "");
+    }
+    sortByDate: function() {
+      this.set("sortProperties", ["date"]);
     }
   }
 });
