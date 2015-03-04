8979e8749c1ceff7e58f404ede481f35de73bb23
Alister Fenix <a.fenix@icloud.com>
2015-03-03 16:56:36 -0800

    Add 'date' component.

--- store.js
+++ store.js
@@ -2,18 +2,25 @@ var beets = [
     {
       id: '1',
       user: "@bitter_end",
-      message: "I really like bitter, cuz it's a place I can be myself."
+      message: "I really like Bitter, cuz it's a place I can be myself.",
+      date: ""
     },
-
     {
       id: '2',
       user: "@bite_me",
-      message: "Every night, every day... i'm so stinking sick of it!"
+      message: "Every night, every day... I'm so stinking sick of it!",
+      date: ""
     },
-
     {
       id: '3',
       user: "@bugz_sux",
-      message: "I don't understand why camels even exist, slow, munchy, clobbery, so what with the H2O retention?"
+      message: "Camels why? You are slow, munchy, clobbery, so what with the H2O retention?",
+      date: ""
+    },
+    {
+      id:'4',
+      user: "@enough",
+      message: "Cut your nails AT HOME.",
+      date: ""
     }
 ]
